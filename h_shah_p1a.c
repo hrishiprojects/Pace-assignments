af7fd91845abc3c0342dea0a785ab56b6e95a329
hrishiprojects <142743430+hrishiprojects@users.noreply.github.com>
2024-02-06 10:46:13 -0500

    Add files via upload

---
+++ h_shah_p1a.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+int main() {
+
+    int final = 0; 
+    int p;
+  for ( p = 1; p <= 100; ++p) {  
+    final += p;  
+  }
+
+  cout << "The sum of integers from 1 to 100 is: " << final << endl;  
+
+  return 0;  
+
+
+}
